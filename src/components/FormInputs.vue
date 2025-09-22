98eb3be05ae77334dae2a13c8c8fd236fdc23e7b
Laurent Bodelet <laurent.bodelet@alchimie.com>
2025-09-22 09:15:17 +0200

    Ajout des nouveaux tests

---
+++ src/components/FormInputs.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const textInput = ref('')
+const textareaInput = ref('')
+const checkboxInput = ref(false)
+const radioInput = ref('')
+const selectInput = ref('')
+</script>
+
+<template>
+  <form>
+    <!-- TODO: Lier ces inputs avec v-model -->
+    <input type="text" />
+    <textarea></textarea>
+    <input type="checkbox" />
+
+    <input type="radio" value="one" />
+    <input type="radio" value="two" />
+
+    <select>
+      <option value="a">A</option>
+      <option value="b">B</option>
+      <option value="c">C</option>
+    </select>
+  </form>
+</template>
